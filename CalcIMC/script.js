4e9ea62fd4f85567f570ef8555073015f5c3957c
AnthonyLoche <anthonylocheifc@gmail.com>
2023-09-18 18:52:12 -0300

    Mim de papai

--- CalcIMC/script.js
+++ CalcIMC/script.js
@@ -1,20 +1,13 @@
-let peso = 0;
-let altura = 0;
 let imc = 0;
 
-function registrarPeso() {
-    const inputElement = document.getElementById('peso');
-    peso += (inputElement.value).replace(",",".");
-    console.log(peso)
-}
-
-function registrarAltura() {
-    const inputElement = document.getElementById('altura');
-    altura += (inputElement.value).replace(",",".");
-    console.log(altura)
-}
-
 function calc(){
-    imc += (peso / (altura ** 2))
-    console.log((imc).toFixed(1).replace(".",","))
+ const peso = (document.getElementById("peso").value).replace(",",".");
+ const altura = (document.getElementById("altura").value).replace(",",".");
+
+ console.log(peso)
+ console.log(altura)
+
+ imc += (peso / (altura ** 2));
+console.log(imc)
+
 }

31154dba66f89c8c449791c0a4796c78af53498f
AnthonyLoche <anthonylocheifc@gmail.com>
2023-10-03 19:34:19 -0300

    Realização de Tabuada e Filtro

---
+++ Tabuada_Filtro/script.js
@@ -0,0 +1,31 @@
+function tabuada() {
+    const valor = document.getElementById("tabuada-input").value;
+    if(valor === "") {
+        document.getElementById("valor").style.animation = "opacityInverse 2s";
+        document.getElementById("valor").style.display = "none";   
+    }
+    else{
+    let valores = [];
+    
+    for (let i = 1; i < 11; i++) {
+        valores.push(`${valor} x ${i} = ` + (valor * i));
+    }
+    document.getElementById("valor").style.display = "block";
+    document.getElementById("valor").style.animation = "opacity 2s";
+    document.getElementById("valor").innerHTML = valores.join("<br>")  ;
+    }
+}
+
+function filtro() {
+    const filtro = document.getElementById("filtro-input").value.toLowerCase();
+    const lista = document.getElementsByTagName("li");
+
+    for (const i of lista) {
+        if(i.innerText.toLowerCase().includes(filtro)){
+            i.style.display = "block";
+        }
+        else{
+            i.style.display = "none";
+        }
+    }
+}

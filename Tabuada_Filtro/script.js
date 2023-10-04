c7f64b27c56b0f0dfa56e16e09e550f21fe2c219
AnthonyLoche <anthonylocheifc@gmail.com>
2023-10-04 13:59:45 -0300

    Ajustes

--- Tabuada_Filtro/script.js
+++ Tabuada_Filtro/script.js
@@ -1,18 +1,18 @@
 function tabuada() {
     const valor = document.getElementById("tabuada-input").value;
-    if(valor === "") {
+    if (valor === "") {
         document.getElementById("valor").style.animation = "opacityInverse 2s";
-        document.getElementById("valor").style.display = "none";   
+        document.getElementById("valor").style.display = "none";
     }
-    else{
-    let valores = [];
-    
-    for (let i = 1; i < 11; i++) {
-        valores.push(`${valor} x ${i} = ` + (valor * i));
-    }
-    document.getElementById("valor").style.display = "block";
-    document.getElementById("valor").style.animation = "opacity 2s";
-    document.getElementById("valor").innerHTML = valores.join("<br>")  ;
+    else {
+        let valores = [];
+
+        for (let i = 1; i < 11; i++) {
+            valores.push(`${valor} x ${i} = ` + (valor * i));
+        }
+        document.getElementById("valor").style.display = "block";
+        document.getElementById("valor").style.animation = "opacity 2s";
+        document.getElementById("valor").innerHTML = valores.join("<br>");
     }
 }
 
@@ -21,11 +21,17 @@ function filtro() {
     const lista = document.getElementsByTagName("li");
 
     for (const i of lista) {
-        if(i.innerText.toLowerCase().includes(filtro)){
-            i.style.display = "block";
+        if (filtro == "") {
+            i.style.fontWeight = "normal";
+            i.style.display = "list-item";
+        } 
+        else if (i.innerText.toLowerCase().includes(filtro)) {
+            i.style.display = "list-item";
+            i.style.fontWeight = "bold";
         }
-        else{
+        else {
             i.style.display = "none";
+            i.style.fontWeight = "normal";
         }
     }
 }
